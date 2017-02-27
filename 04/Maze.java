4e6c8a289459c8d1090582aa5662573ebb26d00e
Kerry Chen <kchen36@stuy.edu>
2017-02-27 15:30:08 -0500

    outline maze

---
+++ 04/Maze.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Maze{
+    private char[][]maze;
+    private boolean animate;
+    public Maze(String filename){
+    }
+    public void readMe(String filename){
+	File infile = new File(filename);
+        Scanner inf = new Scanner(text);
+        int lineNumber = 1;
+        while(inf.hasNextLine()){
+            String line = inf.nextLine();
+            System.out.println(line);
+        }       
+    }   
+    }
+}
