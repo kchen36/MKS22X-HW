34d7cfa4b1f293f1414f43031d2a67f7d1d0cda5
kchen36 <kchen36@stuy.edu>
2017-03-06 21:55:31 -0500

    pasture

--- lakeMaking.java
+++ lakeMaking.java
@@ -2,22 +2,42 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class lakeMaking {
-    public int E,N; 
+    public int E vol; 
     public int[][] pasture;
-	public void scanner(file) {
-	    try{
-		//instead of a try/catch, you can throw the FileNotFoundException.
-		File text = new File(file);// can be a path"/full/path/to/file.txt" 
-		Scanner inf = new Scanner(text);
-		pasture= new int[inf.nextInt()][inf.nextInt()];
-		inf.nextInt();
-		for(int y = 0; x < pasture.length;x++){
-		    for(int x = 0; x < pasture[x].length
-		}
-
+    public lakeMaking(file){
+	scanner(file);
+    }
+    public void scanner(file) {
+	try{
+	    //instead of a try/catch, you can throw the FileNotFoundException.
+	    File text = new File(file);// can be a path"/full/path/to/file.txt" 
+	    Scanner inf = new Scanner(text);
+	    pasture= new int[inf.nextInt()][inf.nextInt()];
+	    inf.nextInt();
+	    lineNumber = 1;
+	    while(inf.hasNextLine() & lineNumber < pasture.length){
+		String line = inf.nextLine();
+		pasture[lineNumber] = line.split(" ");
 	    }
-	    catch(FileNotFoundexception e){
+	    stomp(inf.nextInt(),inf.nextInt(),inf.nextInt());
+	    stomp(inf.nextInt(),inf.nextInt(),inf.nextInt());
+	}
+	catch(FileNotFoundexception e){
+	}
+    }
+    public void solve(){
+	inv val= 0;
+	for(int y = 0; y < pasture.length; y ++){
+	    for(int x = 0; x < pasture[y].length;x ++){
+		if(pasture[y][x] < E){
+		    val += E;
+		    val -= pasture[y][x];
+		}
 	    }
 	}
-    
-]
+	vol = val * 72 * 72;
+    }
+    public String toString(){
+	return "" + vol;
+    }
+}
---
+++ lakeMaking.java~
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class lakeMaking {
+    public int E,N; 
+    public int[][] pasture;
+	public void scanner(file) {
+	    try{
+		//instead of a try/catch, you can throw the FileNotFoundException.
+		File text = new File(file);// can be a path"/full/path/to/file.txt" 
+		Scanner inf = new Scanner(text);
+		pasture= new int[inf.nextInt()][inf.nextInt()];
+		inf.nextInt();
+		for(int y = 0; x < pasture.length;x++){
+		    for(int x = 0; x < pasture[x].length
+		}
+
+	    }
+	    catch(FileNotFoundexception e){
+	    }
+	}
+    
+]
