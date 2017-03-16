dfa45b9696b9b231328751387e8148e91a9a4c00
Kerry Chen <kchen36@stuy.edu>
2017-03-16 15:34:13 -0400

    nto wroking

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -6,12 +6,16 @@ public class Quick{
 	return part(data,0,data.length,k);
     }
     public static int part(int[] data, int start, int end,int k){
+	if(start == end){
+	    return data[start];
+	}
 	Random r = new Random();
 	int pivot = r.nextInt(end - start);
 	int val = data[pivot];
 	data[pivot] = data[start];
+	data[start] = val;
 	int holder = 0;
-	int pos = end;
+	int pos = start;
 	for(int x = start + 1; x < end; x ++){
 	    if(data[x] < val){
 		holder = data[x];
@@ -22,20 +26,20 @@ public class Quick{
 	    }
 	}
 	if(pos > k){
-	    return part(data,start,pos,k);
+	    return part(data,start,pos - 1 ,k);
 	}
 	else if(pos < k){
-	    return part(data,pos,end,k);
+	    return part(data,pos + 1,end,k);
 	}
 	else return data[pos];
-    }
+	}
     public static void main(String[] args){
 	int[]ary = { 2, 10, 15, 23, 0,  5};
-	quickselect(ary , 0);
-	quickselect(ary , 1);
-	quickselect(ary , 2);
-	quickselect(ary , 3);
-	quickselect(ary , 4);
-	quickselect(ary , 5);
+	System.out.println(quickselect(ary, 0));
+	System.out.println(quickselect(ary, 1));
+	System.out.println(quickselect(ary, 2));
+	System.out.println(quickselect(ary, 3));
+	System.out.println(quickselect(ary, 4));
+	System.out.println(quickselect(ary, 5));
     }
 }
