54b7bd98fa579eabc3ae5d46fd7bfa572774263d
Kerry Chen <kchen36@stuy.edu>
2017-03-23 15:07:55 -0400

    finished

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -41,45 +41,49 @@ public class Quick{
 	    return part(data,start,pos,k);
 	}
 	else if(pos < k){
-	    return part(data,pos,end,k);
+	    return part(data,e,end,k);
 	}
 	else return data[pos];
     }
-    public static int quicksort(int[] data, int start, int end,int k){
+    public static void quicksort(int[] data){
+	helper(data,0,data.length);
+    }
+    public static void helper(int[] data, int start,int end){
 	if(start == end){
-	    return data[start];
 	}
-	Random r = new Random();
-	int pivot = r.nextInt(end - start);
-	int val = data[pivot + start];
-	data[pivot + start] = data[start];
-	data[start] = val;
-	int holder = 0;
-	int pos = start;
-	for(int x = start + 1; x < end ; x ++){
-	    if(data[x] < val){
-		holder = data[x];
-		data[x] = data[pos + 1];
-		data[pos + 1] = data[pos];
-		data[pos] = holder;
-		pos++;
+	else{
+	    Random r = new Random();
+	    int pivot = r.nextInt(end - start) + start;
+	    int val = data[pivot];
+	    data[pivot] = data[start];
+	    data[start] = val;
+	    int pos = start;
+	    int i = start + 1;
+	    int e = end - 1;
+	    while(i <= e){
+		if(data[i] == val){
+		    i++;
+		}
+		else if(data[i] < val){
+		    swap(data,i,pos + 1);
+		    pos++;
+		    i++;
+		}
+		else{
+		    swap(data,i,e);
+		    e--;
+		}
 	    }
+	    swap(data,start,pos);
+	    helper(data,start,pos);
+	    helper(data,e + 1,end);
 	}
-	if(pos > k){
-	    return part(data,start,pos,k);
-	}
-	else if(pos < k){
-	    return part(data,pos,end,k);
-	}
-	else return data[pos];
     }
     public static void main(String[] args){
-	int[]ary = { 2, 10, 15, 23, 0,  5};
-    System.out.println(quickselect(ary, 0));
-	System.out.println(quickselect(ary, 1));
-	System.out.println(quickselect(ary, 2));
-	System.out.println(quickselect(ary, 3));
-	System.out.println(quickselect(ary, 4));
-	System.out.println(quickselect(ary, 5));
+	int[]ary = { 2, 2, 2, 2, 2, 2};
+	quicksort(ary);
+	for(int x = 0;x < ary.length;x ++){
+	    System.out.println(ary[x]);
+	}
     }
 }
