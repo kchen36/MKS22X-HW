5dee91321ec8bb8b157d1715c1c695c55edc38b6
kchen36 <kchen36@stuy.edu>
2017-03-26 16:39:28 -0400

    Merge
    works

--- 08/Merge.java
+++ 08/Merge.java
@@ -2,23 +2,55 @@ public class Merge{
     public Merge(){
     }
     public static void mergesort(int[] ary){
-	if(ary.length == 1){
+	if(ary.length <= 1){
 	}
 	else{
 	    int[] left,right;
 	    left = new int[ary.length/2];
-	    right = new int[ary.length - left];
+	    right = new int[ary.length - left.length];
 	    for(int x = 0;x < left.length;x ++){
-		    left[x] = ary[x]
+		left[x] = ary[x];
 	    }
 	    for(int x = 0;x < right.length;x++){
 		right[x] = ary[x + left.length];
 	    }
 	    mergesort(left);
 	    mergesort(right);
-	    int[] destination = new int[left.length + right.length]; 
-	    merging(left,right,destination);
+	    merging(left,right,ary);
 	}
     }
-    public static void merging(int[]
+    public static void merging(int[] left, int[] right, int[] destination){
+	int indexl = 0;
+	int indexr = 0;
+	while(indexl + indexr < destination.length){
+	    if(indexl < left.length && indexr<right.length){
+		if(left[indexl] > right[indexr]){
+		    destination[indexr + indexl] = right[indexr];
+		    indexr++;
+		}
+		else{
+		    destination[indexr + indexl] = left[indexl];
+		    indexl++;
+		}
+	    }
+	    else{
+		if(indexl == left.length){
+		    destination[indexr + indexl] = right[indexr];
+		    indexr++;
+		}
+		else{
+		    destination[indexr + indexl] = left[indexl];
+		    indexl++;
+		}
+	    }
+	}
+    }
+    public static void main (String[] args){
+        int[]ary = { 2, 10, 15, 23, 0,  5};
+	mergesort(ary);
+	for(int x = 0; x < ary.length;x ++){
+	    System.out.println(ary[x]);
+	}
+	
+    }
 }
---
+++ 08/Merge.java~
@@ -0,0 +1,24 @@
+public class Merge{
+    public Merge(){
+    }
+    public static void mergesort(int[] ary){
+	if(ary.length == 1){
+	}
+	else{
+	    int[] left,right;
+	    left = new int[ary.length/2];
+	    right = new int[ary.length - left];
+	    for(int x = 0;x < left.length;x ++){
+		    left[x] = ary[x]
+	    }
+	    for(int x = 0;x < right.length;x++){
+		right[x] = ary[x + left.length];
+	    }
+	    mergesort(left);
+	    mergesort(right);
+	    int[] destination = new int[left.length + right.length]; 
+	    merging(left,right,destination);
+	}
+    }
+    public static void merging(int[]
+}
