f57c45080a028eaf53045a5d227381d1f46b99f2
Kerry Chen <kchen36@stuy.edu>
2017-03-30 15:29:03 -0400

    almost done

--- 09/MyLinkedList.java
+++ 09/MyLinkedList.java
@@ -1,33 +1,79 @@
 public class MyLinkedList{
-    LNode start, end;
+    LNode start, end, current;
+    
     int size;
-    public MyLinkedList{
-
+    public MyLinkedList(){
+	
     }
-    boolean add(int value) {
-	try{
-	    if(size > 0){
-		LNode n = new LNode(value);
-		end.setNext(n);
-		end = end.next();
-		size ++;
-		return true;
-	    }
-	    else{
-		LNode n = new LNode(value);
-		start = n;
-		end = start;
-		return true;
-	    }
+    public boolean add(int value) {
+	if(size > 0){
+	    LNode n = new LNode(value);
+	    end.setNext(n);
+	    end = end.next();
+	    size ++;
+	    return true;
 	}
-	catch (){
+	else{
+	    LNode n = new LNode(value);
+	    start = n;
+	    end = start;
+	    current = start;
+	    return true;
 	}
     }
-    int size(){
+    public int size(){
 	return size;
     }
-    String toString(){
-	String s = "["
+    public int get(int x){
+	if(x < 0 || x >= size){
+	    throw new IndexOutOfBoundsException();
+	} 
+	else{
+	    LNode temp = start;
+	    int y = 0;
+	    while(y < x){
+		temp = temp.next();
+	    }
+	    return temp.value;
+	}
+    }
+    public int set(int index, int newVal){
+	if(x < 0 || x >= size){
+	    throw new IndexOutOfBoundsException();
+	} 
+	else{
+	    LNode temp = start;
+	    int y = 0;
+	    while(y < x){
+		temp. = temp.next();
+	    }
+	    int val = temp.value;
+	    temp.value = newVal
+		return temp.value;
+	}
+    }
+    public String toString(){
+	String s = "[";
+	LNode temp = start;
+	for (int x = 0; x < size;x ++){
+	    s += temp.value;
+	    s += ", ";
+	    temp = temp.next();
+	}
+	s+= "]";
+	return s;
+    }
+    public int indexOf(int value) {
+	LNode temp = start;
+	for (int x = 0; x < size;x ++){
+	    if(temp.value == value){
+		return x;
+	    }
+	    temp = temp.next();
+	}
+	return -1;
+    }
+    public void add(int index, int value) {
     }
     public class LNode{
 	int value;
@@ -48,4 +94,4 @@ public class MyLinkedList{
 	    previous = x;
 	}
     }
-]
+}
