aad638715b92fb22a0e279e859f862fa8266791f
kchen36 <kchen36@stuy.edu>
2017-04-24 19:57:49 -0400

    fat stacks

---
+++ 11/StackCalc.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class StackCalc{
+    public StackCalc(){
+    }
+    public static boolean isOp(String x){
+	return x.equals("+") || x.equals("-") || x.equals("*") || x.equals("/") || x.equals("%");
+    }
+    public static double compute(String func,Double val, double val2){
+	if(func.equals("+")) return val2 + val;
+	if(func.equals("-")) return val2 - val;
+	if(func.equals("*")) return val2 * val;
+	if(func.equals("%")) return val2 % val;
+	else return val2 % val;
+    }
+    public static double eval(String a){
+	String[] token = a.split(" ");
+	Stack<Double> b = new Stack<Double>();
+	for(int x = 0; x < token.length; x ++){
+	    if(isOp(token[x])){
+		b.push(compute(token[x],b.pop(),b.pop()));
+	    }
+	    else{
+		b.push(Double.parseDouble(token[x]));
+	    }
+	}
+	return b.pop();
+    }
+    public static void main(String[] args){
+	System.out.println(StackCalc.eval("10 2 +"));//12.0
+	System.out.println(StackCalc.eval("10 2 -"));//8.0
+	System.out.println(StackCalc.eval("10 2.0 +"));//12.0
+	System.out.println(StackCalc.eval("11 3 - 4 + 2.5 *"));//30.0
+	System.out.println(StackCalc.eval("8 2 + 99 9 - * 2 + 9 -"));//839.0
+	System.out.println(StackCalc.eval("10 2 + 10 * 1 + 1 1 1 + + + 10 10 + -"));//104.0
+    }
+}
---
+++ 11/StackCalc.java~
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Maze{
+    private char[][]maze;
+    private boolean animate;
+    public Maze(String filename){
+	readMe(filename);
+    }
+    public void readMe(String filename){
+	File infile = new File(filename);
+        Scanner inf = new Scanner(text);
+        int lineNumber = 1;
+        while(inf.hasNextLine()){
+            String line = inf.nextLine();
+	    lineNumber++;
+            System.out.println(line);
+        }
+	int length = line.length();
+    }   
+}
