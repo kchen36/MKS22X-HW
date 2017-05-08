2ca49af5eb9c01191dcb0f1a82cb7b171a77ac23
kchen36 <kchen36@stuy.edu>
2017-05-07 20:53:26 -0400

    forgot to push

--- 13/MyHeap.java
+++ 13/MyHeap.java
@@ -5,13 +5,13 @@ public class MyHeap{
     public MyHeap(){
 	ary = new String[10];
 	size = 0;
-	constant = 1;
+	constant = -1;
     }
     public MyHeap(boolean a){
 	size = 0;
 	ary = new String[10];
-	if(a) constant = 1;
-	else constant = -1;
+	if(a) constant = -1;
+	else constant = 1;
     }
     private void resize(){
 	String[] ary2 = new String[ary.length * 2];
@@ -34,7 +34,7 @@ public class MyHeap{
     }
     private void pushUp(){
 	String x = ary[size];
-	for(int i = size; i > 0; i/=2){
+	for(int i = size; i > 1; i/=2){
 	    if(ary[i/2].compareTo(ary[i]) * constant > 0){
 		ary[i] = ary[i/2];
 		ary[i/2] = x;
@@ -52,21 +52,58 @@ public class MyHeap{
     } 
     private void pushDown(){
 	int x = 1;
-        while(x < size){
+        while(x * 2 < size){
 	    String val = ary[x];
 	    String bigger;
 	    int dir;
-	    if(ary[x * 2].compareTo(ary[x * 2 + 1]) * constant > 0){
-		bigger = ary[x * 2];
-		dir = 0;
+	    if(x* 2 < size && x * 2 + 1 <= size){
+		if(ary[x * 2].compareTo(ary[x * 2 + 1]) * constant > 0){
+		    bigger = ary[x * 2];
+		    dir = 0;
+		}
+		else{
+		    bigger = ary[x * 2 + 1];
+		    dir = 1;
+		}
+		if(ary[x * 2 + dir].compareTo(ary[x]) * constant > 0){
+		    ary[x] = ary[x * 2 + dir];
+		    ary[x * 2 + dir] = val;
+		    x = x * 2 + dir;
+		}
+		else break;
 	    }
-	    else{
-		bigger = ary[x * 2 + 1];
-		dir = 1;
+	    else if(x* 2 <= size && ary[x * 2].compareTo(ary[x]) * constant > 0){
+		ary[x] = ary[x * 2];
+		ary[x * 2] = val;
+		x = x * 2;
 	    }
-	    ary[x] = ary[x * 2 + dir];
-	    ary[x * 2 + dir] = val;
-	    x = x * 2 + dir;
+	    else if(x* 2 + 1 <= size && ary[x * 2 + 1].compareTo(ary[x]) * constant > 0){
+		ary[x] = ary[x * 2];
+		ary[x * 2] = val;
+		x = x * 2;
+	    }
+	    else break;
 	}
+    }
+    public String toString(){
+	String s = "";
+	for(int x = 1; x<= size; x++){
+	    s += ary[x] + " ";
+	}
+	return s;
+    }
+    public static void main(String[] args){
+	MyHeap a = new MyHeap();
+	a.add("a");
+	a.add("b");
+	a.add("e");
+	a.add("c");
+	a.add("d");
+	a.remove();
+	a.remove();
+	a.remove();
+	a.remove();
+	a.remove();
+	System.out.println(a);
     }
 }
