35139ca63a6a01882ef819018ac13572c8c2c461
kchen36 <kchen36@stuy.edu>
2017-05-22 22:21:55 -0400

    maze

--- 15/MazeSolver.java
+++ 15/MazeSolver.java
@@ -60,51 +60,92 @@ public class MazeSolver extends Maze{
 	    Location l = f.next();
 	    r = l.getr();
 	    c = l.getc();
-	    board.set(r,c,'@');
+	    board.set(r,c,'.');
 	    if(inBound(r - 1, c)){
 		if(r - 1 == er && c == ec){
+		    board.set(r - 1,c,'@');
+		    back(l);
 		    break;
 		}
 		else {
 		    int d = = Math.abs(er - (r - 1)) + Math.abs(ec - c);
+		    board.set(r - 1,c,'?');
 		    Location n = new Location(r - 1,c,l,l.gets() + 1, d, astar);
 		    f.add(n);
 		}
 	    }
 	    if(inBound(r, c - 1)){
 		if(r == er && c - 1 == ec){
+		    board.set(r,c - 1,'@');
+		    back(l);
 		    break;
 		}
 		else {
 		    int d = = Math.abs(er - r) + Math.abs(ec - (c - 1));
+		    board.set(r,c - 1,'?');
 		    Location n = new Location(r,c - 1,l,l.gets() + 1, d, astar);
 		    f.add(n);
 		}
 	    }
 	    if(inBound(r + 1, c)){
 		if(r + 1 == er && c == ec){
+		    board.set(r + 1,c,'@');
+		    back(l);
 		    break;
 		}
 		else {
 		    int d = = Math.abs(er - (r + 1)) + Math.abs(ec - c);
+		    board.set(r + 1,c,'?');
 		    Location n = new Location(r + 1,c,l,l.gets() + 1, d, astar);
 		    f.add(n);
 		}
 	    }
 	    if(inBound(r, c + 1)){
 		if(r == er && c + 1 == ec){
+		    board.set(r,c + 1,'@');
+		    back(l);
 		    break;
 		}
 		else {
 		    int d = = Math.abs(er - r) + Math.abs(ec - (c + 1));
+		    board.set(r,c + 1,'?');
 		    Location n = new Location(r,c + 1,l,l.gets() + 1, d, astar);
 		    f.add(n);
 		}
 	    }
+	    if(animate){
+		board.toString(25);
+	    }
 	} 
+    }
+    private void back(Location l){
+	if(l.prev() != null){
+	    board.set(l.getr,l.getc,'@');
+	    back(l.prev());
+	}
+	if(animate){
+	    maze.toString(25);
+	}
     }
     public static void main(String[] args){
 	MazeSolver ms = new MazeSolver("data4.txt");
 	ms.solve(2);
+    }
+    public String toString(){
+	int maxr = maze.length;
+	int maxc = maze[0].length;
+	String ans = "";
+	for(int i = 0; i < maxr * maxc; i++){
+	    int row = i/maxc;
+	    int col = i%maxc;
+
+	    char c =  maze[row][col];
+	    ans+=c;
+	    if( col == maxc-1 ){
+		ans += "\n";
+	    }
+
+	}
+	return ans + "\n";
     }
 }
