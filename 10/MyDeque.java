45539009245c1c1fa806a2f7785b259b63065dc5
kchen36 <kchen36@stuy.edu>
2017-06-05 20:40:29 -0400

    reworked
    failed many times decided to restart and switched ways.
    similar to jeremy's way after working with him on his during free

--- 10/MyDeque.java
+++ 10/MyDeque.java
@@ -1,114 +1,107 @@
+// work with Jeremy for 1 period and decided to try his way after my kept screwing up
 import java.util.*;
 public class MyDeque{
     public int front, end, size;
     public String[] ary;
     public MyDeque(){
-	ary = new String[10];
+	ary= new String[10];
+	front = 1;
+	end = -1;
 	size = 0;
-	front = 0;
-	end = 0;
     }
     private void expand(){
-	String[] array = new String[ary.length*2];
-	for(int x = front; x < ary.length;x ++){
-	    array[x-front] = ary[x];
+	String[] array = new String[size * 2];
+	for(int x = 0; x <= end; x++){
+	    array[b] = ary[x];
 	}
-	int x;
-	for(x = 0;x <front; x ++){
-	    array[front + x] = ary[x];
+	if (front > end){
+	    for(int y = 0; y <= ary.length - front - 1; y++){
+		array[array.length - (y + 1)] = ary[ary.length - (y + 1)];
+	    }
 	}
-	front = 0;
-	end = ary.length - 1;
+	front = front + ary.length;
 	ary = array;
     }
     public void addFirst(String a){
 	if(a == null){
 	    throw new NullPointerException();
 	}
-	else if(size == 0){
-	    ary[0] = a;
-	    front = ary.length - 1;
-	    end = 1;
-	}
-	else if(front == end){
+	if(size>=ary.length){
 	    expand();
-	    addFirst(a);
 	}
-	else{
-	    ary[front] = a;
-	    if(front == 0){
-		front = ary.length - 1;
-	    }
-	    front --;
+	front-=1;
+	if(front<0){
+	    front=ary.length-1;
 	}
-	size ++;
+	ary[front]=a;
+	if(size==1){
+	    end=front;
+	}
+	size++;
     }
     public void addLast(String a){
 	if(a == null){
 	    throw new NullPointerException();
 	}
-	else if(size  == 0){
-	    ary[0] = a;
-	    front = ary.length - 1;
-	    end = 1;
-	}
-	else if(front == end){
+	if(size>=ary.length){
 	    expand();
-	    addLast(a);
 	}
-	else{
-	    ary[end] = a;
-	    if(end == ary.length - 1){
-		end = 0;
-	    }
-	    end ++;
+	end+=1;
+	if(end>=ary.length){
+	    end=0;
 	}
-	size ++;
-    }
-    public String removeLast(){
-	if(front == end){
-	    throw new NoSuchElementException();
+	ary[end]=a;
+	if(size==1){
+	    front=end;
 	}
-	else{
-	    if(end == 0){
-		end = ary.length - 1;
-	    }
-	    end = end - 1;
-	    String a = ary[end];
-	    ary[end] = null;
-	    size --;
-	    return a;
-	}
+	size++;
     }
     public String removeFirst(){
-	if(front == end){
+	if(size == 0){
 	    throw new NoSuchElementException();
 	}
 	else{
-	    if (front == ary.length - 1){
+	    String ans = ary[front];
+	    ary[front] = null;
+	    front++;
+	    size--;
+	    if(front >= ary.length){
 		front = 0;
 	    }
-	    front = front ++;
-	    String a = ary[end];
+	    if(size == 0){
+		front = end;
+	    }
+	    return ans;
+	}
+    }
+    public String removeLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	else{
+	    String ans = ary[end];
 	    ary[end] = null;
-	    size --;
-	    return a;
+	    end--;
+	    size--;
+	    if(end < 0){
+		end = ary.length - 1;
+	    }
+	    if(size == 0){
+	    end = front;
+	    }
+	    return ans;
 	}
     }
     public String getFirst(){
-	if(front == end){
+	if(size == 0){
 	    throw new NoSuchElementException();
 	}
-	else{
-	    return ary[front - 1];
-	}
+	return ary[front];
     }
     public String getLast(){
-	if(front == end){
+	if(size == 0){
 	    throw new NoSuchElementException();
 	}
-	else{
-	    return ary[end - 1];
-	}
+	return ary[end];
     }
 }
